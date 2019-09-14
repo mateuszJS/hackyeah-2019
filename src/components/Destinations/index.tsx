3445802f1cfb9b546680bfab95536aedbd7d0a53
mbrzoz <brzozowska.magdalena.m@gmail.com>
2019-09-14 23:48:44 +0200

    Update style filter

--- src/components/Destinations/index.tsx
+++ src/components/Destinations/index.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
 import queryString from 'query-string';
+import { makeStyles } from '@material-ui/styles';
+//import classnames from 'classnames';
+import colors from '../../colors';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+  destination: {
+
+  },
+
+  title: {
+    textAlign: 'center',
+    margin: '40px 20px 20px',
+    color: colors.baseColor,
+    fontWeight: 600,
+  },
+
+});
+
 
 const Destinations = ({ location }: RouteComponentProps) => {
+  const classes = useStyles();
   const { tags: stringifiedString } = queryString.parse(location.search)
   const tags = typeof stringifiedString === 'string'
     ? stringifiedString.split(',')
     : [];
   return (
-    <div>
-      Destinations
+    <div className={classes.destination}>
+      <Typography variant="h4" className={classes.title}>Destinations</Typography>
+
       {tags.map(tag => (
         <p key={tag}>{tag}</p>
       ))}
